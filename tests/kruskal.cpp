8092b9133ec592458843c3f61f1e563e81125b44
Emiliano_Salazar <emisalazar568@gmail.com>
2025-02-11 23:58:32 -0600

    kruskalcpp

--- tests/kruskal.cpp
+++ tests/kruskal.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "../src/Kruskal.h" 
+#include "../src/Kruskal.h"
 
 class KruskalTest : public ::testing::Test {
 protected:
@@ -20,7 +20,7 @@ TEST_F(KruskalTest, GrafoPequeno) {
     g->AddEdge(2, 3, 3);
     g->AddEdge(3, 4, 4);
     
-    EXPECT_EQ(g->KruskalMST(), 10);  
+    EXPECT_EQ(g->KruskalMST(), 10);
 }
 
 TEST_F(KruskalTest, GrafoConectadoConCiclo) {
@@ -28,17 +28,17 @@ TEST_F(KruskalTest, GrafoConectadoConCiclo) {
     g->AddEdge(1, 2, 3);
     g->AddEdge(2, 3, 4);
     g->AddEdge(3, 4, 5);
-    g->AddEdge(4, 0, 1);  
+    g->AddEdge(4, 0, 1);  // Crea un ciclo
 
-    EXPECT_EQ(g->KruskalMST(), 10);  
+    EXPECT_EQ(g->KruskalMST(), 10);
 }
 
 TEST_F(KruskalTest, GrafoDesconectado) {
     g->AddEdge(0, 1, 5);
     g->AddEdge(1, 2, 10);
     
-    // Un nodo 3 y 4 sin conexión
-    EXPECT_ANY_THROW(g->KruskalMST());  
+    // Nodo 3 y 4 sin conexión
+    EXPECT_ANY_THROW(g->KruskalMST());  // Debería fallar por grafo desconectado
 }
 
 TEST_F(KruskalTest, GrafoDenso) {
@@ -50,5 +50,5 @@ TEST_F(KruskalTest, GrafoDenso) {
     g->AddEdge(1, 3, 3);
     g->AddEdge(2, 4, 4);
 
-    EXPECT_EQ(g->KruskalMST(), 10);  
+    EXPECT_EQ(g->KruskalMST(), 10);
 }
